c30b62aa1c9b1c695b9a8af418bbf1499389ddfa
itsOwlbit <{ID}+{username}@user.noreply.github.comgit config user.email {ID}+{username}@user.noreply.github.com>
2023-07-03 14:26:04 -0700

    Now error inside manage.py

--- stoqs/compose/local/stoqs/stoqs-start.sh
+++ stoqs/compose/local/stoqs/stoqs-start.sh
@@ -72,7 +72,7 @@ fi
 if [ "$PRODUCTION" == "false" ]; then
     export MAPSERVER_SCHEME=http
     echo "Starting development server with DATABASE_URL=${DATABASE_URL}..."
-    ${STOQS_SRVPROJ}/manage.py runserver 0.0.0.0:8000 --settings=config.settings.local
+    ${STOQS_SRVHOME}/manage.py runserver 0.0.0.0:8000 --settings=config.settings.local
 else
     echo "Starting production server with DATABASE_URL=${DATABASE_URL}..."
     # For testing on port 8000 before certificate is in place make a security exception in your browser
