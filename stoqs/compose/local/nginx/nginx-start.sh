4037eb81d04afbc6089f2efdaff9f8b6f6b10bc5
nadz101 <93563211+nadz101@users.noreply.github.com>
2023-07-06 15:18:27 -0700

    changes

--- stoqs/compose/local/nginx/nginx-start.sh
+++ stoqs/compose/local/nginx/nginx-start.sh
@@ -12,4 +12,4 @@ if [ -z "$NGINX_SERVER_NAME" ]; then
   export NGINX_CRT_NAME=localhost
   export NGINX_KEY_NAME=localhost
 fi
-DOLLAR='$' envsubst < ${NGINX_TMPL} > /etc/nginx/nginx.conf
+envsubst < ${NGINX_TMPL} > /etc/nginx/nginx.conf
