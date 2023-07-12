b75b87a2459515eb479074654fd84613e36a5d6d
itsOwlbit <{ID}+{username}@user.noreply.github.comgit config user.email {ID}+{username}@user.noreply.github.com>
2023-07-12 15:49:46 -0700

    more removal of unneeded changes

--- stoqs/stoqs/loaders/loadTestData.py
+++ stoqs/stoqs/loaders/loadTestData.py
@@ -24,13 +24,13 @@ MBARI Dec 28, 2011
 import os
 import datetime
 from CANON import CANONLoader
-import stoqs.loaders.timing
+import timing
 
 # Assign input data sources - use locally served x3d terrain data
 cl = CANONLoader('default', 'Initial Test Database',
                     description = 'Post-setup load of a variety of data to use for testing',
                     x3dTerrains = {
-                            '/stoqs/static/x3d/Monterey25_10x/Monterey25_10x_scene.x3d': {
+                            '/static/x3d/Monterey25_10x/Monterey25_10x_scene.x3d': {
                                 'position': '-2822317.31255 -4438600.53640 3786150.85474',
                                 'orientation': '0.89575 -0.31076 -0.31791 1.63772',
                                 'centerOfRotation': '-2711557.94 -4331414.32 3801353.46',
