85583bf8f4ae9cbfa9f4d24b31f1e8ccd016c6ef
itsOwlbit <{ID}+{username}@user.noreply.github.comgit config user.email {ID}+{username}@user.noreply.github.com>
2023-07-20 20:02:27 -0700

    adding drew's debugpy

--- stoqs/manage.py
+++ stoqs/manage.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
+import debugpy
 from pathlib import Path
+
+## DEBUGPY
+from django.conf import settings
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
@@ -27,5 +31,18 @@ if __name__ == "__main__":
     # stoqs directory.
     current_path = Path(__file__).resolve()
     sys.path.append(str(current_path / "stoqs"))
+
+    print('arg1: {}'.format(sys.argv[1]))
+    ####### ADDDING DEBUGPY ########
+    # $ This will only activate the debugger when runserver arg is passed. Uncomment to run debugger all the time
+
+    if settings.DEBUG and not os.getenv("RUN_MAIN") and os.getenv("DEBUG") == '1':
+        debugpy.listen(("0.0.0.0", 3000))
+    ## BLOCK EXECUTION AND WAIT FOR DEBUGGER uncomment if you want it to wait for you to connect vscod
+        print('Awaiting debugger')
+        debugpy.wait_for_client()
+        print('Attached!')
+
+    ## END DEBUGPY INSERT
 
     execute_from_command_line(sys.argv)
